78c3bb411219d913fd1ea0be3a2ef1ef0400e1dd
IDKx66 <2372623859@qq.com>
2025-06-21 16:31:00 +0800

    添加了非递归前序遍历（用栈）

--- Data_Structures/Binary Tree.cpp
+++ Data_Structures/Binary Tree.cpp
@@ -1,6 +1,7 @@
 //二叉树的链式存储结构  
 #include<iostream>
 #include<queue>
+#include<stack>
 using namespace std;
 
 typedef char ElemType;
@@ -14,6 +15,55 @@ typedef struct TreeNode{
 } TreeNode;
 
 typedef struct TreeNode *BiTree;//BiTree是指向结构体TreeNode的指针类型   struct可省略
+
+// 二叉树节点指针栈定义
+typedef struct{
+    BiTree *data;  // 存储BiTree指针的数组
+    int top;
+} TreeStack;
+
+#define MAXSIZE 100
+
+// 初始化树节点栈
+TreeStack* initTreeStack(){
+    TreeStack *s = new TreeStack;
+    s->data = new BiTree[MAXSIZE];
+    s->top = -1;
+    return s;
+}
+
+// 判断树节点栈是否为空
+int isEmptyTree(TreeStack *s){
+    if(s->top == -1){
+        return 1;
+    }
+    else return 0;
+}
+
+// 树节点进栈
+int pushTree(TreeStack *s, BiTree e){
+    if(s->top >= MAXSIZE - 1){
+        cout << "栈满了" << endl;
+        return 0;
+    }
+
+    s->top++;
+    s->data[s->top] = e;
+    return 1;
+}
+
+// 树节点出栈
+int popTree(TreeStack *s, BiTree *e){
+    if(s->top == -1){
+        cout << "栈为空" << endl;
+        return 0;
+    }
+
+    *e = s->data[s->top];
+    s->top--;
+    return 1;
+}
+
 
 //先序创造二叉树
 BiTree createBiTree(){
@@ -68,6 +118,35 @@ void levelOrder(BiTree T){
     }
 }
 
+// 非递归前序遍历
+void iterPreOrder(BiTree T){
+    if(T == nullptr) return;
+    TreeStack *s = initTreeStack();
+    while(T != nullptr || !isEmptyTree(s)){
+        while(T != nullptr){
+            cout << T->data << " ";
+            pushTree(s, T);
+            T = T->lchild;
+        }
+
+        popTree(s, &T);
+        T = T->rchild;
+    }
+}
+
+
+//复制二叉树
+void copyTree(BiTree T, BiTree &newT){
+    if(T == nullptr){
+        newT = nullptr;
+        return;
+    }
+    newT = new TreeNode;
+    newT->data = T->data;
+    copyTree(T->lchild, newT->lchild);
+    copyTree(T->rchild, newT->rchild);
+}
+
 int main(){
 
     // 手动构造如下二叉树：
@@ -102,5 +181,13 @@ int main(){
     cout << "层序遍历：";
     levelOrder(root);
     cout << endl;
+
+    cout << "非递归前序遍历：";
+    iterPreOrder(root);
+    cout << endl;
+
+    // cout << "中序遍历非递归：";
+    // inOrderNonRecursive(root);
+    // cout << endl;
     return 0;
 }
